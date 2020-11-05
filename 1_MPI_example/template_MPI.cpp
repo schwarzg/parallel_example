b505fdc1952ff51756f5c787e66b564dc85f9466
schwarzg <schwarzg1104@gmail.com>
2020-11-05 15:41:27 +0900

    template_MPI add

---
+++ 1_MPI_example/template_MPI.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<mpi.h>
+
+int main(int argc,char* argv[])
+{
+
+	//MPI start
+	int nproc,rank=0;
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&nproc);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	
+	//Prepare system
+	int n_ensemble;	
+		
+	//Ensemble loop - Parallelized!
+	for ( int i = rank; i < n_ensemble; i += nproc )
+	{
+	
+		/******************************
+				Calculate here
+		*******************************/
+		
+	}
+	
+	//MPI finish
+	MPI_Finalize();
+	return 0;
+}
