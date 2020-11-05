8645d0e4aaad94aad987991820dc5975d19bc417
schwarzg <schwarzg1104@gmail.com>
2020-11-05 16:07:29 +0900

    Modeify ensemble average template

--- 1_MPI_example/template_MPI.cpp
+++ 1_MPI_example/template_MPI.cpp
@@ -4,23 +4,22 @@
 int main(int argc,char* argv[])
 {
 
-	//MPI start
+	/*     MPI start    */
 	int nproc,rank=0;
 	MPI_Init(&argc,&argv);
 	MPI_Comm_size(MPI_COMM_WORLD,&nproc);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	
-	//Prepare system
+	/*     System setting    */
 	int n_ensemble;	
 		
 	//Ensemble loop - Parallelized!
 	for ( int i = rank; i < n_ensemble; i += nproc )
 	{
 	
-		/******************************
-				Calculate here
-		*******************************/
-		
+		/*     Calculate here    */
+
+	
 	}
 	
 	//MPI finish
