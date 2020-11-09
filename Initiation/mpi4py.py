cd7ead1715253dda029daf765aa7ede81d671318
schwarzg <schwarzg1104@gmail.com>
2020-11-09 16:22:23 +0900

    mpi4py example update

--- Initiation/mpi4py.py
+++ Initiation/mpi4py.py
@@ -1,2 +1,10 @@
-import mpi4py as mpi
+from mpi4py import MPI
+#MPI initiated from import of mpi4py
 
+comm = MPI.COMM_WORLD
+nproc = comm.Get_size()
+rank = comm.Get_rank()
+
+print("I am %d of %d",(nproc,rank))
+
+#MPI finish at the end of program automatically
